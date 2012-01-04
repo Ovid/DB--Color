d954fc0afeec03df49101b198b273261c2c21877
Curtis Poe <curtis@weborama.com>
2012-01-04 08:26:14 +0100

    Bump version number and document Build.PL change

--- lib/DB/Color.pm
+++ lib/DB/Color.pm
@@ -11,11 +11,11 @@ DB::Color - Colorize your debugger output
 
 =head1 VERSION
 
-Version 0.03
+Version 0.04
 
 =cut
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 =head1 SYNOPSIS
 
--- lib/DB/Color/Highlight.pm
+++ lib/DB/Color/Highlight.pm
@@ -12,11 +12,11 @@ DB::Color::Highlight - Provides highlighting for DB::Color
 
 =head1 VERSION
 
-Version 0.03
+Version 0.04
 
 =cut
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 BEGIN {
     no warnings 'redefine';
